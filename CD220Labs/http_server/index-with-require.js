3217bd73bac0d7ad6e2116e672ed6a56cac72d05
gau-tam-byte <gautamkumark1214@gamil.com>
2023-01-26 13:29:11 -0500

    new

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -4,17 +4,17 @@ const today = require('./today');
 const requestListener = function (req, res) {
   res.writeHead(200);
   var dateval = today.getDate();
-  var greet= "its still not morning"
+  var greett= "its still not morning"
   if(dateval.getHours()> 6 && dateval.getHours()<12 ){
-      greet="Good Morning!"
+      greett="Good Morning!"
   }else if(dateval.getHours()>12 && dateval.getHours()<18){
-      greet="Good AfterNoon!"
+      greett="Good AfterNoon!"
   }else if(dateval.getHours()>=18 && dateval.getHours()<21){
-      greet="Good Evening!"
+      greett="Good Evening!"
   }else if(dateval.getHours()>=21 && dateval.getHours<24){
-      greet="Good Night!"
+      greett="Good Night!"
   }
-  res.end(`Hello, World! Todays date and time ! ${today.getDate()} ${greet}`);
+  res.end(`Hello, World! Todays date and time ! ${today.getDate()} ${greett}`);
 }
 
 const port = 8080;
